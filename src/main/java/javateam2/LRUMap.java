d2d8b7178d5db53a3199dd0fb3b459834daf37e4
Takako Imaizumi <wantnothingbutgreen@gmail.com>
2011-12-14 16:24:11 +0900

    merge atosh

--- src/main/java/javateam2/LRUMap.java
+++ src/main/java/javateam2/LRUMap.java
@@ -1,25 +1,30 @@
 package javateam2;
 
+import java.util.Date;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class LRUMap<E, F> {
 	private int maxSize;
-	private int discardTime; //秒
+	private long discardTime; //秒
+	//private long lastAccess;
+	private Date lastAccess = null;
 	private HashMap<E, F> hashMap = new HashMap<E, F>();
 	// キーを管理するリスト 先頭から使われていない順に並んでいる
 	private ArrayList<E> LRUList = new ArrayList<E>();
-	
-	public LRUMap(int size, int time){
+
+	public LRUMap(int size, long time){
 		if(time < 0){
 			throw new IllegalArgumentException("消去時間は0秒以上:input="+time);
 		}
-		discardTime = time;
+		discardTime = time * 1000;
+		this.lastAccess = new Date();
 		if(size < 1){
 			throw new IllegalArgumentException("サイズは1以上:input="+size);
 		}
 		maxSize = size;
 	}
+
 	public LRUMap(int size){
 		if(size < 1){
 			throw new IllegalArgumentException("サイズは1以上:input="+size);
@@ -28,9 +33,11 @@ public class LRUMap<E, F> {
 		discardTime = 0;
 	}
 	public int size(){
+		discardOld();
 		return maxSize;
 	}
 	public void put(E key, F value){
+		discardOld();
 		if(LRUList.indexOf(key) != -1){
 			// キーが重複した場合は上書き
 			updateKey(key,value);
@@ -42,6 +49,9 @@ public class LRUMap<E, F> {
 		hashMap.put(key, value);
 	}
 	public F get(E key){
+		//現在時刻が最後のアクセスからdiscardTimeを超えていれば一個捨てる
+		discardOld();
+
 		if(hashMap.get(key) == null){
 			// マップにないキーを指定された場合はnullを返す
 			return null;
@@ -50,7 +60,18 @@ public class LRUMap<E, F> {
 		LRUList.add(key);
 		return hashMap.get(key);
 	}
-	
+
+	// 時間制限がきたら古いのを捨てる
+	private void discardOld() {
+		if (this.discardTime != 0) {
+			Date now = new Date();
+			if (now.getTime() >= this.lastAccess.getTime() + this.discardTime) {
+				this.remove(this.LRUList.get(0));
+			}
+			this.lastAccess = now;
+		}
+	}
+
 	private void putNewKey(E key, F value){
 		if(LRUList.size() >= maxSize)
 		{
@@ -62,7 +83,7 @@ public class LRUMap<E, F> {
 	private void updateKey(E key, F value){
 		remove(key);
 	}
-	
+
 	private void remove(E key){
 		// リストとマップからキーを消す
 		// リストに指定キーが無ければ何もしない
@@ -70,9 +91,10 @@ public class LRUMap<E, F> {
 			hashMap.remove(key);
 			LRUList.remove(LRUList.indexOf(key));
 		}
-	}	
+	}
 
 	public void resize(int newSize) {
+		discardOld();
 		if (newSize < maxSize) {
 			for (int diffSize = maxSize - newSize; diffSize > 0; diffSize--) {
 				remove(LRUList.get(0));
--- src/test/java/javateam2/LRUMapTest.java
+++ src/test/java/javateam2/LRUMapTest.java
@@ -9,34 +9,34 @@ public class LRUMapTest {
 		LRUMap<Integer, String> lruMap = new LRUMap<Integer, String>(3);
 		assertEquals(3, lruMap.size());
 	}
-	
+
 	@Test
 	public void testConstructorType1() {
 		LRUMap<Integer, String> lruMap = new LRUMap<Integer, String>(3);
 		lruMap.put(1, "1");
 		assertEquals("1", lruMap.get(1));
 	}
-	
+
 	@Test
 	public void testConstructorType2() {
 		LRUMap<String, Double> lruMap = new LRUMap<String, Double>(3);
 		lruMap.put("hoge", 1.5);
 		assertEquals(1.5, lruMap.get("hoge"));
 	}
-	
+
 	@Test
 	public void testPut() {
 		LRUMap<Integer, String> lruMap = new LRUMap<Integer, String>(3);
 		lruMap.put(1,"test001");
 		assertEquals("test001", lruMap.get(1));
 	}
-	
+
 	@Test
 	public void testSize() {
 		LRUMap<Integer, String> lruMap = new LRUMap<Integer, String>(1);
 		lruMap.put(1,"test002");
 		lruMap.put(2,"test003");
-		assertEquals(null, lruMap.get(1));		
+		assertEquals(null, lruMap.get(1));
 	}
 	@Test
 	public void testSize3() {
@@ -91,10 +91,10 @@ public class LRUMapTest {
 		assertEquals("2", lruMap.get(2));
 		assertEquals("second", lruMap.get(1));
 	}
-	
+
 	@Test
 	public void testResize() {
-		LRUMap<Integer, String> lruMap =  new LRUMap<Integer, String>(3);
+		LRUMap<Integer, String> lruMap = new LRUMap<Integer, String>(3);
 		lruMap.put(1,"1st");
 		lruMap.put(2,"2nd");
 		lruMap.put(3,"3rd");
@@ -103,17 +103,17 @@ public class LRUMapTest {
 	}
 	@Test
 	public void testResize2() {
-		LRUMap<Integer, String> lruMap =  new LRUMap<Integer, String>(2);
+		LRUMap<Integer, String> lruMap = new LRUMap<Integer, String>(2);
 		lruMap.put(1,"1st");
 		lruMap.put(2,"2nd");
 		lruMap.resize(1);
 		assertEquals(1, lruMap.size());
 		assertEquals(null, lruMap.get(1));
 		assertEquals("2nd", lruMap.get(2));
-	} 
+	}
 	@Test
 	public void testResize3() {
-		LRUMap<Integer, String> lruMap =  new LRUMap<Integer, String>(3);
+		LRUMap<Integer, String> lruMap = new LRUMap<Integer, String>(3);
 		lruMap.put(1,"1st");
 		lruMap.put(2,"2nd");
 		lruMap.put(3,"3rd");
@@ -123,15 +123,30 @@ public class LRUMapTest {
 		assertEquals("4th", lruMap.get(4));
 	}
 	@Test
-	public void testDiscardAsTime() {
-		LRUMap<Integer, String> lruMap =  new LRUMap<Integer, String>(3, 5);
+	public void testDiscardWithTime() {
+		LRUMap<Integer, String> lruMap = new LRUMap<Integer, String>(3, 1);
 		lruMap.put(1,"1st");
 		lruMap.put(2,"2nd");
 		lruMap.put(3,"3rd");
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 		}
-		assertEquals(null, lruMap.get(1));			
+		assertEquals(null, lruMap.get(1));
+	}
+
+	// 2倍の時間経過したら2つ消えるテスト
+	@Test
+	public void testDiscardWithTime2() {
+		LRUMap<Integer, String> lruMap = new LRUMap<Integer, String>(3, 1);
+		lruMap.put(1,"1st");
+		lruMap.put(2,"2nd");
+		lruMap.put(3,"3rd");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+		}
+		assertEquals(null, lruMap.get(1));
+		assertEquals(null, lruMap.get(2));
 	}
 }
